db5359e24db79993ec1be9d9600a9e233b604890
Amit Kumar <amitmac2@gmail.com>
2015-09-23 18:35:31 +0530

    update StateMachine.py

--- StateMachine.py
+++ StateMachine.py
@@ -9,7 +9,8 @@ class StateMachine:
         temp = agenda.pop()
         current_state, index = temp[0], temp[1]
         while True:
-            if index == len(tape) - 1 and current_state in self.final_states:
+            # print current_state,index,self.final_states
+            if index == len(tape) and current_state in self.final_states:
                 return True
             else:
                 agenda = agenda + self.new_states(current_state,index,tape)
@@ -18,29 +19,26 @@ class StateMachine:
                 else:
                     temp = agenda.pop()
                     current_state, index = temp[0], temp[1]
-        
-        if accept_state(current_state):
-            return True
-        else:
-            return False
 
     def new_states(self,current_state,index,tape):
         agenda = []
+        # print (current_state,index)
         for state in self.transition_table[current_state][tape[index]]:
-            agenda.append([state,index+1])
+            if state != -1:
+                agenda.append([state,index+1])
         for state in self.transition_table[current_state]['E']:
-            if state != '-1':
-            	agenda.append([state,index])
+            if state != -1:
+                agenda.append([state,index])
         
         return agenda
 
 initial_state = 0
 final_states = [4]
-transition_table = {0:{'b':[1],'a':-1,   '!':-1, 'E':'-1'},
-                    1:{'b':-1, 'a':[2],  '!':-1, 'E':'-1'},
-                    2:{'b':-1, 'a':[2,3],'!':-1, 'E':'-1'},
-                    3:{'b':-1, 'a':-1,   '!':[4],'E':'-1'},
-                    4:{'b':-1, 'a':-1,   '!':-1, 'E':'-1'}}
+transition_table = {0:{'b':[1],  'a':[-1],   '!':[-1], 'E':[-1]},
+                    1:{'b':[-1], 'a':[2],  '!':[-1], 'E':[-1]},
+                    2:{'b':[-1], 'a':[2,3],'!':[-1], 'E':[-1]},
+                    3:{'b':[-1], 'a':[-1],   '!':[4],'E':[-1]},
+                    4:{'b':[-1], 'a':[-1],   '!':[-1], 'E':[-1]}}
 tape = "baaaa!"
 sm = StateMachine(initial_state,transition_table,final_states)
 accepted = sm.ndfsa(tape)
